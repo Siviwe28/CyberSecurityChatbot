617aba52ac03f3b4619af91481ba2dbc11964fb6
Siviwe <ST10062453@rcconnect.edu.za>
2026-04-11 10:35:51 +0200

    Added RespondHandler class

--- CyberSecurityChatbot/Chatbot.cs
+++ CyberSecurityChatbot/Chatbot.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CyberSecurityChatbot
+class Chatbot
 {
-    internal class Chatbot
+    public static void StartChat(string userName)
     {
+        while (true)
+        {
+            Display.AskQuestion();
+            string input = Console.ReadLine().ToLower();
+
+            if (!InputValidator.IsValid(input))
+            {
+                Console.WriteLine("⚠️ Please enter something.");
+                continue;
+            }
+
+            if (input.Contains("exit"))
+            {
+                Console.WriteLine($"Goodbye, {userName}! Stay safe online 👋");
+                break;
+            }
+
+            RespondHandler.Handle(input);
+        }
     }
 }
