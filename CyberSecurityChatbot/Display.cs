7ba4224d80d47edaf0e5fdc58f83a7676022bbb3
Siviwe <ST10062453@rcconnect.edu.za>
2026-04-11 10:26:55 +0200

    Add AudioPlayer class for voice greeting playback

--- CyberSecurityChatbot/Display.cs
+++ CyberSecurityChatbot/Display.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CyberSecurityChatbot;
+using System;
 
-namespace CyberSecurityChatbot
+class Display
 {
-    internal class Display
+    public static string GetUserName()
     {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        if (!InputValidator.IsValid(name))
+        {
+            name = "User";
+        }
+
+        Console.WriteLine($"\nWelcome, {name}! 👋");
+        TypeEffect("I’m here to help you stay safe online.\n");
+
+        return name;
+    }
+
+    public static void TypeEffect(string message)
+    {
+        foreach (char c in message)
+        {
+            Console.Write(c);
+            System.Threading.Thread.Sleep(20);
+        }
+    }
+
+    public static void AskQuestion()
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("\nAsk me something: ");
+        Console.ResetColor();
     }
 }
