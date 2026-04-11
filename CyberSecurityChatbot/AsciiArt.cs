325e5ea2c8c3b3da0b5166092df5f861c90952aa
Siviwe <ST10062453@rcconnect.edu.za>
2026-04-11 10:15:44 +0200

    Add AsciiArt class for chatbot logo display

--- CyberSecurityChatbot/AsciiArt.cs
+++ CyberSecurityChatbot/AsciiArt.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CyberSecurityChatbot
+class AsciiArt
 {
-    internal class AudioPlayer
+    public static void Show()
     {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+
+        Console.WriteLine(@"
+   ____       _                _   _                 
+  / ___| ___ | | ___  _ __ ___| |_(_) ___  _ __  ___ 
+ | |    / _ \| |/ _ \| '__/ __| __| |/ _ \| '_ \/ __|
+ | |___| (_) | | (_) | |  \__ \ |_| | (_) | | | \__ \
+  \____|\___/|_|\___/|_|  |___/\__|_|\___/|_| |_|___/
+
+      CYBERSECURITY AWARENESS BOT
+");
+
+        Console.ResetColor();
     }
 }
--- CyberSecurityChatbot/AudioPlayer.cs
+++ CyberSecurityChatbot/AudioPlayer.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Media;
 
-namespace CyberSecurityChatbot
+class AudioPlayer
 {
-    internal class AudioPlayer
+    public static void PlayGreeting()
     {
+        try
+        {
+            SoundPlayer player = new SoundPlayer("greeting.wav");
+            player.PlaySync();
+        }
+        catch
+        {
+            Console.WriteLine("⚠️ Audio file not found.");
+        }
     }
 }
