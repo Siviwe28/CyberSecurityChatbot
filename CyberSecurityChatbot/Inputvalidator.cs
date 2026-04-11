e7f5fd74d6db3fa076b95eb7373a180436ec5a89
Siviwe <ST10062453@rcconnect.edu.za>
2026-04-11 10:30:54 +0200

    Added display classes

--- CyberSecurityChatbot/Inputvalidator.cs
+++ CyberSecurityChatbot/Inputvalidator.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CyberSecurityChatbot
+class InputValidator
 {
-    internal class Inputvalidator
+    public static bool IsValid(string input)
     {
+        return !string.IsNullOrWhiteSpace(input);
     }
 }
