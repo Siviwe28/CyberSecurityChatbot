6ec4ff90527ab29e6c4d4a1f2648ed9bb22c8436
Siviwe <ST10062453@rcconnect.edu.za>
2026-04-11 10:33:13 +0200

    Added an input validator

--- CyberSecurityChatbot/RespondHandler.cs
+++ CyberSecurityChatbot/RespondHandler.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CyberSecurityChatbot
+class RespondHandler
 {
-    internal class RespondHandler
+    public static void Handle(string input)
     {
+        if (input.Contains("how are you"))
+        {
+            Console.WriteLine("I'm functioning perfectly! 😄");
+        }
+        else if (input.Contains("purpose"))
+        {
+            Console.WriteLine("I help users stay safe online.");
+        }
+        else if (input.Contains("what can i ask"))
+        {
+            Console.WriteLine("Ask about passwords, phishing, or safe browsing.");
+        }
+        else if (input.Contains("password"))
+        {
+            Console.WriteLine("Use strong passwords with letters, numbers, and symbols.");
+        }
+        else if (input.Contains("phishing"))
+        {
+            Console.WriteLine("Avoid suspicious emails and links.");
+        }
+        else if (input.Contains("browsing"))
+        {
+            Console.WriteLine("Only use secure websites (HTTPS).");
+        }
+        else
+        {
+            Console.WriteLine("I didn’t quite understand that. Could you rephrase?");
+        }
     }
 }
